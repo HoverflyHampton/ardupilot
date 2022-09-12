8817ef1f6384989fa7825f4f8770c1be7a1dff66
William Hampton <william.hampton@hoverflytech.com>
2022-09-12 10:36:39 -0400

    Fix: preland bug

--- libraries/AC_PrecLand/AC_PrecLand.cpp
+++ libraries/AC_PrecLand/AC_PrecLand.cpp
@@ -114,6 +114,15 @@ const AP_Param::GroupInfo AC_PrecLand::var_info[] = {
     // @RebootRequired: True
     AP_GROUPINFO("LAG", 9, AC_PrecLand, _lag, 0.02f), // 20ms is the old default buffer size (8 frames @ 400hz/2.5ms)
 
+    // @Param: OUT_REJ
+    // @DisplayName: Outlier Reject Count
+    // @Description: The number of outlier data samples before accepting outliers
+    // @Range: 1 50
+    // @Increment: 1
+    // @Units: N/A
+    // @User: Advanced
+    // @RebootRequired: True
+    AP_GROUPINFO("OUT_REJ", 10, AC_PrecLand, _max_outlier_reject_count, 3),
     AP_GROUPEND
 };
 
@@ -354,7 +363,7 @@ void AC_PrecLand::run_estimator(float rangefinder_alt_m, bool rangefinder_alt_va
                 } else {
                     float NIS_x = _ekf_x.getPosNIS(_target_pos_rel_meas_NED.x, xy_pos_var);
                     float NIS_y = _ekf_y.getPosNIS(_target_pos_rel_meas_NED.y, xy_pos_var);
-                    if (MAX(NIS_x, NIS_y) < 3.0f || _outlier_reject_count >= 3) {
+                    if (MAX(NIS_x, NIS_y) < 3.0f || _outlier_reject_count >= _max_outlier_reject_count) {
                         _outlier_reject_count = 0;
                         _ekf_x.fusePos(_target_pos_rel_meas_NED.x, xy_pos_var);
                         _ekf_y.fusePos(_target_pos_rel_meas_NED.y, xy_pos_var);
