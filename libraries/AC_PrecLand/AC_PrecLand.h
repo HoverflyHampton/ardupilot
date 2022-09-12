c6f0b2b837ecd29d412f5e67a8d4c82a832fe998
William Hampton <william.hampton@hoverflytech.com>
2022-09-12 10:36:39 -0400

    Fix: preland bug

--- libraries/AC_PrecLand/AC_PrecLand.h
+++ libraries/AC_PrecLand/AC_PrecLand.h
@@ -131,7 +131,7 @@ private:
 
     PosVelEKF                   _ekf_x, _ekf_y;     // Kalman Filter for x and y axis
     uint32_t                    _outlier_reject_count;  // mini-EKF's outlier counter (3 consecutive outliers lead to EKF accepting updates)
-    
+    AP_Int32                    _max_outlier_reject_count; // mini-EKF's max outlier before accepting values
     Vector3f                    _target_pos_rel_meas_NED; // target's relative position as 3D vector
 
     Vector2f                    _target_pos_rel_est_NE; // target's position relative to the IMU, not compensated for lag
